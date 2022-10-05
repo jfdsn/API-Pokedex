abe565b268b0badeaa4f42021125264c6600a284
Jonathan Faria <jonathanfdsn@gmail.com>
2022-10-05 15:30:52 -0300

    Inicio do projeto

---
+++ src/routes.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const routes = express.Router();
+
+//metodos get
+routes.get('/pokemon', async (req, res) => {
+    
+});
+
+routes.get('/pokemon/:id', async (req, res) => {
+
+});
+
+//metodo post
+routes.post('/pokemon', async (req, res) => {
+
+});
+
+//metodo put
+routes.put('/pokemon/:id', async (req, res) => {
+
+});
+
+//metodo delete
+routes.delete('/pokemon/:id', async (req, res) => {
+
+});
+
+module.exports = routes; 
